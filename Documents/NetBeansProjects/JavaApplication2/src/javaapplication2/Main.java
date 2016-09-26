5f30444e432f8b75b7fa2e61ad94eb5d8efca6a8
richard-fasthub <richard.robert@fasthub.co.tz>
2016-09-26 15:31:44 +0300

    test

---
+++ Documents/NetBeansProjects/JavaApplication2/src/javaapplication2/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String aString = "Richard";
+        System.out.println("My name is " + aString);
+    }
+}
