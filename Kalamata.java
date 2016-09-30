b8542d64f10404755f9361a719ae22d1636d3004
richard-fasthub <richard.robert@fasthub.co.tz>
2016-09-30 18:03:12 +0300

    JAVA Essential Training

---
+++ Kalamata.java
@@ -0,0 +1,15 @@
+
+package pkginterface;
+
+/**
+ *
+ * @author Richie
+ */
+
+public class Kalamata extends Olive {
+
+    public Kalamata() {
+        super(OliveName.KALAMATA, OliveColor.PURPLE, 2);
+    }
+
+}
---
+++ Ligurian.java
@@ -0,0 +1,17 @@
+
+package pkginterface;
+
+/**
+ *
+ * @author Richie
+ */
+
+    public class Ligurian extends Olive {
+
+    public Ligurian() {
+        super(OliveName.LIGURIAN, OliveColor.BLACK, 5);
+    }
+
+}
+    
+}
