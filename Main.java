28283c184e41a7bd28be1d9ea2ce51b8ebd8720d
richard-fasthub <richard.robert@fasthub.co.tz>
2016-09-30 10:40:27 +0300

    JAVA Essential Training

--- Main.java
+++ Main.java
@@ -1,6 +1,37 @@
-public class Main{
-	public static void maim(String[] args){
-	System.out.println("hello Richard");
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olive;
+
+/**
+ *
+ * @author Richie
+ */
+
+    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Olive> olives = new ArrayList<>();
+        olives.add(new Olive(OliveName.KALAMATA, OliveColor.PURPLE, 74));
+        olives.add(new Olive(OliveName.KALAMATA, OliveColor.PURPLE, 74));
+        olives.add(new Olive(OliveName.LIGURIAN, OliveColor.BLACK, 29));
+        olives.add(new Olive(OliveName.LIGURIAN, OliveColor.BLACK, 29));
+        olives.add(new Olive(OliveName.LIGURIAN, OliveColor.BLACK, 29));
+        olives.add(new Olive(OliveName.LIGURIAN, OliveColor.BLACK, 29));
+
+        OlivePress press = new OlivePress();
+        int totalOil = press.getOil(olives);
+
+        System.out.println("You got " + totalOil + " units of oil");
+
+    }
 
 }
 
